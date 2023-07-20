a0c70cbda0de8494161f1ef10b2a65af1401b479
Nick Sheward <nickshwrd@gmail.com>
2023-07-20 09:29:57 +0100

    Installed EJS view engine and provided new index file as response

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const __dirname = new URL('.', import.meta.url).pathname;
 // Set up an Express app
 const app = express();
 
+// Register view engine (EJS), app.set allows configuration of settings
+// Default looks in the views dir
+app.set('view engine', 'ejs');
+
 // Listen for requests, local host inferred
 app.listen(3000);
 
@@ -14,9 +18,21 @@ app.listen(3000);
 app.get('/', (req, res) => {
   // Express method, no need to set header and infers status code (200)
   // .sendFile looks for absolute path, second argument (object) states the relative root
-  res.sendFile('./views/index.html', { root: __dirname });
+  // When using views, use the render method and file name minus the extension
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
   res.sendFile('./views/about.html', { root: __dirname });
+});
+
+// Redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+// 404 redirect, middleware, don't need to scope out to URL if code has not matched up to this point
+// Must go at bottom of file and manually add 404 error code
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
 });
